55722f878a60797f237360e6e23705ca17c6e572
Andreas Bauer <Andreas.Bauer.de@gmx.net>
2020-03-18 15:46:45 +0100

    added normalized arrows to vizualize_angles

--- edge_detection_by_bene.py
+++ edge_detection_by_bene.py
@@ -8,29 +8,36 @@ import scipy.optimize as opt
 import clickpoints
 import matplotlib
 import copy
+import sys
+sys.path.append("/home/user/Software/fiber-orientation")
+
+from plotting import vizualize_angles
 
 
 def project_angle(a):
-    # projects angles in range [-pi,pi] to [0,pi/2] to extract measure of parallelicity
+    # projects angles in range [0,2pi] to [0,pi/2] to extract measure of parallelicity
     angle=copy.deepcopy(a)
     if isinstance(angle,(float,int)):
         angle = np.array([angle])
     if not isinstance(angle,np.ndarray):
         angle = np.array(angle)
-    a_adjusted = np.abs(angle).astype(float)  #################### please check previous angle analysis
-    a_adjusted[a_adjusted > (np.pi / 2)] = np.pi  -  a_adjusted[a_adjusted > (np.pi / 2)]
-    return a_adjusted
+     #################### please check previous angle analysis
+
+    angle[angle > np.pi ] = np.pi  -  angle[angle > np.pi ] # mapping all values > np.pi
+    angle[angle > np.pi/2] = np.pi/2 - angle[angle > np.pi/2] # mapping all values > np.pi/2
+    return angle
 
 
 def calculate_angle(vec1, vec2, axis=0):
     v1 = np.expand_dims(vec1, axis=0) if len(np.array(vec1).shape) == 1 else np.array(vec1)
     v2 = np.expand_dims(vec2, axis=0) if len(np.array(vec2).shape) == 1 else np.array(vec2)
 
-    # angles of vector in [0,2pi] to the x axis
-    angles1 = np.arctan2(np.take(v1, 1, axis), np.take(v1, 0, axis)) + np.pi # xycoordinates are reversed
-    angles2 = np.arctan2(np.take(v2, 1, axis), np.take(v2, 0, axis)) + np.pi # xycoordinates are reversed
-    # angles of vectors towards each other in [0,2pi]
-    angle = angles1-angles2
+    # angles towards x axis in range [pi,-pi]
+    angles1 = np.arctan2(np.take(v1, 1, axis), np.take(v1, 0, axis)) # xycoordinates are reversed
+    angles2 = np.arctan2(np.take(v2, 1, axis), np.take(v2, 0, axis))# xycoordinates are reversed
+    # angle between vector in range [0,2pi]
+    angle = (angles1-angles2) % (2 * np.pi)  # not sure how this works exactely
+    # taking the modulo should be the same as +2pi if angle <0 which should  be correct......
     return angle
 
 
@@ -78,17 +85,9 @@ l_vecs = np.array([[l[0][-1]-l[0][0],l[1][-1]-l[1][0]] for l in all_lines])
 l_points = np.array([[l[0][0],l[1][0]] for l in all_lines])
 l_lenght = np.linalg.norm(l_vecs,axis=1)
 c_vecs = np.array([[center[0]-p[0],center[0]-p[1]] for p in l_points])
-
-
-
-
-
-
-
-
-
-ta=calculate_angle(l_vecs,c_vecs,axis=1)
+ta = calculate_angle(l_vecs,c_vecs,axis=1)
 ta_adjusted = project_angle(ta)
 
-vizualize_angles(ta_adjusted, l_points, l_vecs,c_vecs,image=im )
+vizualize_angles(ta, l_points, l_vecs,c_vecs,image=im )
 
+vizualize_angles(ta_adjusted, l_points, l_vecs,c_vecs ,image=im ,sample_factor=5, size_factor=10)
--- plotting.py
+++ plotting.py
@@ -2,19 +2,22 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-def vizualize_angles(angles, points, vecs1, vecs2, image=None, normalize=True, norm_factor=0.1, sample_factor=10):
+def vizualize_angles(angles, points, vecs1, vecs2, image=None, normalize=True, size_factor=10, sample_factor=10,cbar_max_angle=None):
     plt.figure()
     if isinstance(image, np.ndarray):
         plt.imshow(image)
     # normalization and creating a color range
-    colors = matplotlib.cm.get_cmap("Greens")((angles - 0) / (np.pi / 2 - 0))
+    colors = matplotlib.cm.get_cmap("Greens")(angles/np.max(angles))
     for i,(p,v1,v2,c) in enumerate(zip(points,vecs1,vecs2,colors)):
         if i % sample_factor == 0:
+            if normalize:
+                v1 = v1 * size_factor / np.linalg.norm(v1)
+                v2 = v2 * size_factor / np.linalg.norm(v2)
             plt.arrow(p[0], p[1], v1[0], v1[1], head_width=5, color="C0")
             plt.arrow(p[0], p[1], v2[0], v2[1], head_width=5, color="C1")
             plt.scatter(p[0],p[1],color=c)
             plt.text(p[0], p[1], str(i) + "\n" + str(np.round(angles[i],2)))
-
-    norm = matplotlib.colors.Normalize(vmin=0, vmax=np.pi / 2)
+    vmax = cbar_max_angle if isinstance(cbar_max_angle,(int,float)) else np.max(angles)
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=vmax)
     sm = plt.cm.ScalarMappable(cmap=matplotlib.cm.get_cmap("Greens"), norm=norm)
     plt.colorbar(sm)
